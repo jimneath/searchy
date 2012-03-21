28934892d205d617795c38e77e5d248167b14f7e
Jim Neath <jimneath@googlemail.com>
2012-03-21 19:46:25 +0000

    Multiword searching

--- lib/searchy/search.rb
+++ lib/searchy/search.rb
@@ -2,30 +2,39 @@ module Searchy
   module Search
     extend ActiveSupport::Concern
     TYPES = [:string, :text]
+    attr_accessor :query_parts
     
     module ClassMethods
       def search(query = nil)
-        query.present? ? where([search_command, search_arguments]) ? scoped
+        self.query = query
+        search_scope
       end
       
       protected 
       
-      def search_command
-        *Array.new(searchable_columns.length).map{"%#{query}%"}
+      def query=(str)
+        self.query_parts = str.strip.split(/\s+/)
+      end
+      
+      def search_scope
+        query.present? ? where([search_command, *search_arguments]) ? scoped
+      end
+      
+      def search_arguments
+        arguments = Array.new(searchable_columns.length).map{"%#{query}%"}
+        Array.new(self.query_parts.length).map{arguments}.flatten
       end
       
       def search_command
-        parts = searchable_columns.map do |field| 
-          "#{table_name}.#{field} LIKE ?"
+        parts = self.query_parts.map do
+          searchable_columns.map{|f| "#{table_name}.#{field} LIKE ?"}.join(' OR ')
         end
         
-        parts.join(' OR ')
+        parts.map{|p| "(#{p})"}.join(' AND ')
       end
       
       def searchable_columns
-        @searchable_columns ||= self.columns.select do |column| 
-          TYPES.include?(column.type)
-        end.map(&:name)
+        self.columns.select{|c| TYPES.include?(c.type)}.map(&:name)
       end
     end
   end
--- lib/searchy/version.rb
+++ lib/searchy/version.rb
@@ -1,3 +1,3 @@
 module Searchy
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
